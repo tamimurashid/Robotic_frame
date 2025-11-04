d2273199c13e71cadc0e8bb0186b0650e944b400
tamimurashid <rashidytamimu@gmail.com>
2025-11-04 19:28:53 +0300

    update

--- src/robot_core.cpp
+++ src/robot_core.cpp
@@ -1,11 +1,7 @@
 #include "robot_core.h"
 
-Robotic_frame::Robotic_frame(uint8_t _RX, uint8_t _TX, uint8_t _botbaudRate){
-  RX = _RX;
-  TX = _TX;
-  _botbaudRate = botbaudRate;
-
-}
+Robotic_frame::Robotic_frame(uint8_t _RX, uint8_t _TX, uint8_t _botbaudRate) : RX(_RX), TX(_TX), botbaudRate(_botbaudRate),
+_botSerial(_RX, _TX), motor1(1), motor2(2), motor3(3), motor4(4){}
 
 void Robotic_frame::setMotor(uint8_t _motor_1, uint8_t _motor_2, uint8_t _motor_3, uint8_t _motor_4){
     motor_1 = _motor_1;
@@ -13,11 +9,17 @@ void Robotic_frame::setMotor(uint8_t _motor_1, uint8_t _motor_2, uint8_t _motor_
     motor_3 = _motor_3;
     motor_4 = _motor_4;
 
-    AF_DCMotor motor_1(motor_1);
-    AF_DCMotor motor_2(motor_2);
-    AF_DCMotor motor_3(motor_3);
-    AF_DCMotor motor_4(motor_4);
+    motor1 = AF_DCMotor(motor_1);
+    motor2 = AF_DCMotor(motor_2);
+    motor3 = AF_DCMotor(motor_3);
+    motor4 = AF_DCMotor(motor_4);
+    
+}
 
+
+void Robotic_frame::begin(){
+   _botSerial.begin(botbaudRate);
+   setMotor(motor_1, motor_2, motor_3, motor_4);
 }
 
 
--- src/robot_core.h
+++ src/robot_core.h
@@ -5,21 +5,24 @@
 #include <SoftwareSerial.h>
 #include <AFMotor.h>
 
+
+
+
+
+
 class Robotic_frame{
     private:
-    uint8_t  motor_1;
-    uint8_t  motor_2;
-    uint8_t  motor_3;
-    uint8_t  motor_4;
-    uint8_t  RX;
-    uint8_t  TX;
-    uint8_t  botbaudRate;
+        uint8_t  RX, TX, botbaudRate, motor_1, motor_2, motor_3, motor_4;
+        SoftwareSerial _botSerial;
+        AF_DCMotor  motor1, motor2, motor3, motor4;
+   
 
 
-  public:
-    Robotic_frame(uint8_t _RX, uint8_t _TX, uint8_t _botbaudRate);
-    void begin();
-    void setMotor(uint8_t _motor_1, uint8_t _motor_2, uint8_t _motor_3, uint8_t _motor_4);
+    public:
+        Robotic_frame(uint8_t _RX, uint8_t _TX, uint8_t _botbaudRate);
+        void begin();
+        void setMotor(uint8_t _motor_1, uint8_t _motor_2, uint8_t _motor_3, uint8_t _motor_4);
+    
 
    
 
