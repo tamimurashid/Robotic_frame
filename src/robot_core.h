33a526c13d837eb180bdd66d4d1792179782569a
tamimurashid <rashidytamimu@gmail.com>
2025-11-04 14:32:42 +0300

    update

--- src/robot_core.h
+++ src/robot_core.h
@@ -6,6 +6,23 @@
 #include <AFMotor.h>
 
 class Robotic_frame{
+    private:
+    uint8_t  motor_1;
+    uint8_t  motor_2;
+    uint8_t  motor_3;
+    uint8_t  motor_4;
+    uint8_t  RX;
+    uint8_t  TX;
+    uint8_t  botbaudRate;
+
+
+  public:
+    Robotic_frame(uint8_t _RX, uint8_t _TX, uint8_t _botbaudRate);
+    void begin();
+    void setMotor(uint8_t _motor_1, uint8_t _motor_2, uint8_t _motor_3, uint8_t _motor_4);
+
+   
+
 
 };
 
